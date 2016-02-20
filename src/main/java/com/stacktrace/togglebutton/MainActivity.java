2c88bce92abc034a35e7e4e927a5d8da99c9bcc0
stacktrace828 <stacktrace828@gmail.com>
2016-02-20 23:54:29 +0800

    finish customed draw

---
+++ src/main/java/com/stacktrace/togglebutton/MainActivity.java
@@ -0,0 +1,21 @@
+package com.stacktrace.togglebutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.stacktrace.togglebutton.view.ToggleButton;
+
+public class MainActivity extends Activity {
+    private ToggleButton mToggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToggleButton = (ToggleButton) findViewById(R.id.toggle_button);
+        
+        mToggleButton.setSlideBackgroundResource(R.drawable.slide_button_background);
+        mToggleButton.setSwitchBackgroundResource(R.drawable.switch_background);
+        mToggleButton.setToogleState(ToggleButton.ToggleState.Open);
+    }
+}
---
+++ src/main/java/com/stacktrace/togglebutton/view/ToggleButton.java
@@ -0,0 +1,51 @@
+package com.stacktrace.togglebutton.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ToggleButton extends View{
+
+    private ToggleState mState;
+    private Bitmap mSlideBackground;
+    private Bitmap mSwitchBackground;
+
+    public void setToogleState(ToggleState state) {
+        mState = state;
+    }
+
+    public enum ToggleState{
+       Open, Close
+    }
+
+    public ToggleButton(Context context) {
+        super(context);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setSlideBackgroundResource(int slide_button_background) {
+        mSlideBackground  = BitmapFactory.decodeResource(getResources(), slide_button_background);
+    }
+
+    public void setSwitchBackgroundResource(int switch_background) {
+        mSwitchBackground = BitmapFactory.decodeResource(getResources(), switch_background);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(mSlideBackground.getWidth(), mSlideBackground.getHeight());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mSlideBackground, 0, 0, null);
+    }
+}
